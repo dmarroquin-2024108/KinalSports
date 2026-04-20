f5c96c8110dd47d31d7d6332344a31cdca46ce8a
Informatica <informatica@alumno.local>
2026-04-20 11:33:01 -0600

    Ni funciono pero hay que subirlo

--- client-admin/src/features/users/store/adminStore.js
+++ client-admin/src/features/users/store/adminStore.js
@@ -1,7 +1,10 @@
 import { create } from "zustand";
 import { getFields as getFieldsRequest,
     createField as createFieldRequest,
+    updateField as updateFieldRequest,
+    deleteField as deleteFieldRequest
  } from "../../../shared/apis";
+import { get } from "react-hook-form";
 
 export const useFieldsStore = create((set, get)=>({
     fields: [],
@@ -41,5 +44,40 @@ export const useFieldsStore = create((set, get)=>({
                 error: e.response?.data?.message || "Error al crear la cancha",
             })
         }
+    },
+
+    updateField: async(id, FormData)=>{
+        try{
+            set({loading: true, error: null});
+            
+            const response = await updateFieldRequest(id, FormData);
+            set({
+                fields: get().fields.map((field)=>
+                    field._id === id ? response.data.data : field,
+                ),
+                loading: false,
+            })
+        }catch(e){
+            set({
+                loading: false,
+                error: e.response?.data?.message || "Error al actualizar la cancha",
+            })
+        }
+    },
+
+    deleteField: async(id) =>{
+        try{
+            set({loading: true, error: null});
+            await deleteFieldRequest(id);
+            set({
+                fields: get().fields.filter((field)=> field._id !== id),
+                loading: false
+            })
+        }catch(e){
+          set({
+                loading: false,
+                error: e.response?.data?.message || "Error al eliminar la cancha",
+            })  
+        }
     }
-}))
+})) 
