8d8b0fa65b5edab4c7b619c81245bb2ae361df81
dmarroquin-2024108 <dmarroquin-2024108@kinal.edu.gt>
2026-04-27 08:01:04 -0600

    feat: Agregar Prettier y sus configuraciones

--- authenthication-service/auth-service/src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
+++ authenthication-service/auth-service/src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a514ff90942d0210042c048409f676414de8a949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ee249a4e9c6562849d1b0a8b83ce4c41803231")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
